f30c684fd50da45e98b41e6ef90f8895033e50b9
dadosha <55433356+dadosha@users.noreply.github.com>
2024-07-24 23:37:25 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    let equals = arr1.every((elem) => elem === arr2[arr1.indexOf(elem)]);
+
+    return equals;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (users.filter((user) => user.gender === gender).length === 0) {
+        return 0;
+    }
+    let result = users.filter((user) => user.gender === gender).reduce((result, user, index, arr) => {
+        result += user.age;
+        if (index === arr.length - 1) {
+            return result / arr.length;
+        }
+        return result;
+    }, 0);
+
+    return result;
 }
