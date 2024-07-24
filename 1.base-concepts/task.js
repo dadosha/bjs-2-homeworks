00e5db6da647b43bcb4203f5db7801f3a8bc1ea8
dadosha <55433356+dadosha@users.noreply.github.com>
2024-07-24 06:44:49 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let disc = Math.pow(b, 2) - 4 * a * c;
+  if (disc === 0) {
+    arr[0] = -b / (2 * a);
+  } else if (disc > 0) {
+    arr[0] = (-b + Math.sqrt(disc)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(disc)) / (2 * a);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent /= 100;
+  let creditBody = amount - contribution;
+  let p = percent / 12;
+  let monthPayment = creditBody * (p + (p / (Math.pow(1 + p, countMonths) - 1)));
+  let payment = monthPayment * countMonths
+  return +payment.toFixed(2);
 }
